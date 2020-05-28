a81bcaa46c6dcf08ed75e5a6b21192c70ebdad58
Krutie Patel <krutie.patel@gmail.com>
2020-05-28 23:50:20 +1000

    requireModule(@nuxt/components) added

--- src/nuxt.js
+++ src/nuxt.js
@@ -1,6 +1,9 @@
 import { join } from "path";
 
 export default function() {
+  // Make sure @nuxt/components is available
+  this.requireModule("@nuxt/components");
+
   this.nuxt.hook("components:dirs", (dirs) => {
     // Add ./components dir to the list
     dirs.push({
